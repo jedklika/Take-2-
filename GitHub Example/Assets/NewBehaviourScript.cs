7899838760e2eb4605f4f1e39371f8e66fb366ea
jedklika <43482414+jedklika@users.noreply.github.com>
2021-09-24 16:44:21 -0400

    Revert "new text"
    This reverts commit 6b1b8ccfb036cdd0bcbba3131d34e1f86c1ac8cf.

--- GitHub Example/Assets/NewBehaviourScript.cs
+++ GitHub Example/Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Github is challenging");
+        Debug.Log("Hello Aurora Game Dev @ gsu, Welcome to today's work shop on github");
     }
 
     // Update is called once per frame
